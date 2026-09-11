4641da2301d76f218fd540e9b0ae9d87f4895ae6
Afeef281 <mohammedafeef281@gmail.com>
2026-09-11 19:29:18 +0530

    Time: 15 ms (43.28%), Space: 19.4 MB (45.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         hashstr = {}
-        if len(s) != len(t):
-            return False
-        for i in range(len(s)):
-            if s[i] not in hashstr:
-                hashstr[s[i]]=1
+        if len(s) != len(t):return False
+        for char in s:
+            if char not in hashstr:
+                hashstr[char]=1
             else:
-                hashstr[s[i]] +=1
+                hashstr[char] +=1
         for i in range(len(s)):
-            if t[i] not in hashstr:
-                return False
-            elif hashstr[t[i]] == 0:
+            if t[i] not in hashstr or hashstr[t[i]] == 0:
                 return False
             else:
                 hashstr[t[i]] -=1
