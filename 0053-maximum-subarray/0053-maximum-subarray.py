45e93b4f0186f2f1c846219fa5ea1c3c8cea1481
Afeef281 <mohammedafeef281@gmail.com>
2026-09-22 11:12:25 +0530

    Time: 34 ms (64.11%), Space: 31.3 MB (75.1%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,13 +4,7 @@ class Solution:
         n =len(nums)
         res =float('-inf')
         for i in range(n):
-            # if M <0:
-            #     M =0
-            # if m < 0:
-            #     m =0
             M = M + nums[i]
-            # m = m + nums[n-i-1]
-
             res = max(res ,M)
             if M <0:
                 M =0
