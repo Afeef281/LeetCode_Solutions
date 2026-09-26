38087b943226264ad8a8617fe06a61e481bdf195
Afeef281 <mohammedafeef281@gmail.com>
2026-09-26 21:45:06 +0530

    Time: 3 ms (44.8%), Space: 22.4 MB (68.05%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -9,8 +9,10 @@ class Solution:
             head = head.next
 
         curr = head
+        if head == None:
+            return head
 
-        while curr != None and curr.next != None:
+        while curr.next != None:
             if curr.next.val == val:
                 curr.next = curr.next.next
             else:
